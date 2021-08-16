686fdb8c1b7e85b02b808f9ad65ab040df06444f
victor.costa <vicpc42@gmail.com>
2021-08-16 17:25:40 -0300

    Changes to test scripts. Added test with mosned and changed mcd to use world for analysis

--- test_scripts/vctk/run_test.sh
+++ test_scripts/vctk/run_test.sh
@@ -1,11 +1,15 @@
 #while [ $(basename &(pwd)) -neq td-stargan-vc ]; do cd ..; done
 
+echo $0 $1 $2
+
 model_dir=$1
 out_dir=$2
 
 epoch=100
+device=2
 
 scrip_dir=test_scripts/vctk
+mosnet_dir=../MOSNet
 
 #CUDA_VISIBLE_DEVICES=1 python generate_from_dataset.py --save_path $out_dir/signals/ --load_path $model_dir --data_path datasets/vctk/ --data_file test_files_mcd --epoch $epoch
 
@@ -13,9 +17,13 @@ scrip_dir=test_scripts/vctk
 #cp $model_dir/githash $out_dir
 #echo $epoch > $out_dir/epoch
 
-python $scrip_dir/test_mcd.py --test_path $out_dir/signals/ --save_file $out_dir/dists_mcd
+python $scrip_dir/test_mcd.py --test_path $out_dir/signals/ --save_file $out_dir/mcd_results
 
-python $scrip_dir/test_speaker_rec.py --test_path $out_dir/signals/ --save_file $out_dir/spkrec_results --speechbrain_hparam $scrip_dir/speechbrain_model/sb_classifier_hparams.yaml
+CUDA_VISIBLE_DEVICES=$device python $scrip_dir/test_speaker_rec.py --test_path $out_dir/signals/ --save_file $out_dir/spkrec_results --speechbrain_hparam $scrip_dir/speechbrain_model/sb_classifier_hparams.yaml
+
+CUDA_VISIBLE_DEVICES=$device python $mosnet_dir/custom_test.py --pretrained_model $mosnet_dir/output/mosnet.h5 --rootdir $out_dir/signals/
+mv $out_dir/signals/MOSnet_result_raw.txt $out_dir/mosnet_result_raw.txt
+python $scrip_dir/test_mosnet.py --test_path $out_dir/mosnet_result_raw.txt --save_file $out_dir/mosnet_results
 
 #python test_scripts/get_model_info.py --model_dir $model_dir --save_file $out_dir/info
 
--- test_scripts/vctk/test_mcd.py
+++ test_scripts/vctk/test_mcd.py
@@ -7,15 +7,70 @@ import re
 import numpy as np
 import soundfile as sf
 import librosa
+import pysptk
+import pyworld
 from fastdtw import fastdtw
 from tqdm import tqdm
+
+
+ref_mceps = {}
 
 def eq_rms(signal, target_rms):
     rms = np.sqrt((signal**2).mean())
     gain = 10**(target_rms/20)/rms
     return signal*gain
 
-def mfcc_dist(test_file, ref_file, sr=16000, target_rms=-25):
+def world_analyze(signal, sr=16000):
+    
+    f0_min=50.0
+    f0_max = 500.0
+    nfft=1024
+    hop_len = 5.0 #ms
+    
+    mcep_dim=24
+    mcep_alpha=0.42
+
+    signal = signal.astype('double')
+    #f0, time_axis = pyworld.harvest(signal, sr, f0_floor=f0_min, f0_ceil=f0_max, frame_period=hop_len)
+    _f0, time_axis = pyworld.dio(signal, sr, f0_floor=f0_min, f0_ceil=f0_max, frame_period=hop_len)
+    f0 = pyworld.stonemask(signal, _f0, time_axis, sr)
+    
+    sp = pyworld.cheaptrick(signal, f0, time_axis, sr, fft_size=nfft)  # extract smoothed spectrogram
+    #ap = pyworld.d4c(signal, f0, time_axis, sr, fft_size=nfft)         # extract aperiodicity
+    
+    mcep = pysptk.sp2mc(sp, mcep_dim, mcep_alpha)
+    
+    return mcep, f0
+
+    
+
+def mfcc_dist(test_file, ref_file, sr=16000):
+    #print(ref_mceps.keys())
+    
+    if test_file in ref_mceps.keys():
+        test_mcep = ref_mceps[test_file]
+    else:
+        test_signal,sr = librosa.load(test_file, sr=sr)
+        
+        test_mcep, test_f0 = world_analyze(test_signal, sr)
+        test_mcep = test_mcep[test_f0 > 0] #Remove silence
+    
+    if ref_file in ref_mceps.keys():
+        ref_mcep = ref_mceps[ref_file]
+    else:
+        ref_signal,sr = librosa.load(ref_file, sr=sr)
+        
+        ref_mcep, ref_f0 = world_analyze(ref_signal, sr)
+        ref_mcep = ref_mcep[ref_f0 > 0] #Remove silence
+        
+        ref_mceps[ref_file] = ref_mcep
+    
+    (dist, path) = fastdtw(test_mcep, ref_mcep, dist=2)
+    
+    return dist/len(path)
+
+
+def mfcc_dist_old(test_file, ref_file, sr=16000, target_rms=-25):
     
     test_signal,sr = librosa.load(test_file, sr=sr)
     ref_signal,sr = librosa.load(ref_file, sr=sr)
@@ -51,13 +106,13 @@ def main():
     orig_list.sort()
     #print(orig_list)
     
-    dists={}
+    results={'mcd_result_conv':{}, 'mcd_result_orig':{}}
+    
+    
 
     for src_file in tqdm(orig_list):
         filename, src_spk = re.match('(\S+)_(\S+?)-X_orig.wav',os.path.basename(src_file)).groups()
-        
-        if src_spk not in dists.keys():
-            dists[src_spk] = {}
+
         #print(filename,src_spk)
         conv_list =  glob.glob(os.path.join(args.test_path, f'{filename}_{src_spk}-*_conv.wav'))
         #print(conv_list)
@@ -65,32 +120,25 @@ def main():
             #print(f'{filename}_{src_spk}-(\S+?)_conv.wav',conv_file)
             tgt_spk = re.match(f'{filename}_{src_spk}-(\S+?)_conv.wav',os.path.basename(conv_file)).group(1)
             
-            if tgt_spk not in dists[src_spk].keys():
-                dists[src_spk][tgt_spk] = []
-            
             tgt_file = os.path.join(args.test_path,f'{re.sub(src_spk,tgt_spk,filename)}_{tgt_spk}-X_orig.wav')
             #print(src_file, tgt_file, conv_file)
             
-            dists[src_spk][tgt_spk].append(mfcc_dist(conv_file,tgt_file))
+            results['mcd_result_conv'].setdefault(src_spk,{}).setdefault(tgt_spk,[]).append(mfcc_dist(conv_file,tgt_file))
 
-    dists_orig={}      
     for src_file in tqdm(orig_list):
         filename_src, src_spk = re.match('(\S+)_(\S+?)-X_orig.wav',os.path.basename(src_file)).groups()
-        if src_spk not in dists_orig.keys():
-            dists_orig[src_spk] = {}
+
         #print(filename_src,src_spk)
         for tgt_file in orig_list:
             filename_tgt, tgt_spk = re.match('(\S+)_(\S+?)-X_orig.wav',os.path.basename(tgt_file)).groups()
-            if tgt_spk not in dists_orig[src_spk].keys():
-                dists_orig[src_spk][tgt_spk] = []
+
             if filename_src.split('_')[1] != filename_tgt.split('_')[1]:
                 continue
-            dists_orig[src_spk][tgt_spk].append(mfcc_dist(src_file,tgt_file))
+            results['mcd_result_orig'].setdefault(src_spk,{}).setdefault(tgt_spk,[]).append(mfcc_dist(src_file,tgt_file))
     
     with open(args.save_file,'wb') as f:
-        pickle.dump(dists,f)
-    with open(args.save_file+'_orig','wb') as f:
-        pickle.dump(dists_orig,f)
+        pickle.dump(results,f)
+
     #print(dists)
 
 if __name__ == '__main__':
